0f07e691b5897b00b692f44ad497effc2475d156
Victor Quinn <vquinn@ngpvan.com>
2013-08-11 21:58:19 -0400

    Base implementation of constructors, Table and Dynasty object, starting on the find() method

--- dynasty.js
+++ dynasty.js
@@ -1,11 +1,50 @@
 (function() {
-  var Dynasty;
+  var Dynasty, Table, dynamodb, _;
+
+  dynamodb = require('dynamodb');
+
+  _ = require('underscore');
 
   Dynasty = (function() {
-    function Dynasty() {}
+    function Dynasty(credentials) {
+      if (credentials.region) {
+        credentials.endpoint = "dynamodb." + credentials.region + ".amazonaws.com";
+      }
+      this.ddb = dynamodb.ddb(credentials);
+      this.tables = {};
+    }
+
+    Dynasty.prototype.table = function(name) {
+      return this.tables[name] = this.tables[name] || new Table(this, name);
+    };
 
     return Dynasty;
 
   })();
+
+  Table = (function() {
+    function Table(parent, name) {
+      this.parent = parent;
+      this.name = name;
+    }
+
+    Table.prototype.find = function(opts, callback) {
+      var hash, range;
+      if (callback == null) {
+        callback = null;
+      }
+      if (_.isString(opts)) {
+        hash = opts;
+      } else {
+        hash = opts.hash, range = opts.range;
+      }
+      return console.log([hash, range]);
+    };
+
+    return Table;
+
+  })();
+
+  module.exports = Dynasty;
 
 }).call(this);
--- src/dynasty.coffee
+++ src/dynasty.coffee
@@ -1,2 +1,31 @@
+# Main Dynasty Class
+
+dynamodb = require('dynamodb')
+_ = require('underscore')
+
 class Dynasty
-  constructor: () ->
+
+  constructor: (credentials) ->
+    if credentials.region
+      credentials.endpoint = "dynamodb.#{credentials.region}.amazonaws.com"
+
+    @ddb = dynamodb.ddb credentials
+    @tables = {}
+
+  # Given a name, return a Table object
+  table: (name) ->
+    @tables[name] = @tables[name] || new Table this, name
+
+class Table
+
+  constructor: (@parent, @name) ->
+
+  find: (opts, callback = null) ->
+    if _.isString opts
+      hash = opts
+    else
+      {hash, range} = opts
+
+    console.log [hash, range]
+
+module.exports = Dynasty
