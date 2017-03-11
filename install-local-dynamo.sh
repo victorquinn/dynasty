37edcf785cc6f436e320939b2c9a7387c862be22
Victor Quinn <mail@victorquinn.com>
2017-03-11 14:29:59 -0500

    Three times the charm?

--- install-local-dynamo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p ~/dynamo-local
-cd ~/dynamo-local
-curl -O https://s3-us-west-2.amazonaws.com/dynamodb-local/dynamodb_local_latest.tar.gz
-tar xzvf dynamodb_local_latest.tar.gz
-java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb
